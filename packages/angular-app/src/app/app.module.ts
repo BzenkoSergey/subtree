cdb0363e8c45d72a68f0cbcf41dcd1041b6e0a2a
SergeyBzenko <sergey.bzenko@bhnetwork.com>
2019-05-28 03:41:14 +0300

    initial

--- packages/angular-app/src/app/app.module.ts
+++ packages/angular-app/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { LibModule } from '@harmony102/foo';
+import { LibModule } from '@harmony102/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
